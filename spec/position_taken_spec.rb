809efe627f22e9e323a3e571b4c0ed44bb3cf7c3
Valentina E <tiyahelfrih@gmail.com>
2020-05-20 16:36:47 +0000

    Automatically backed up by Learn

--- spec/position_taken_spec.rb
+++ spec/position_taken_spec.rb
@@ -1,7 +1,7 @@
 require_relative '../lib/position_taken.rb'
 
-def position_taken(input)
-  position(input)="X" || position_taken(input)= "O"
+def position_taken?(input)
+  position_taken?(input)="X" || position_taken?(input)= "O"
 end
   
 describe '#position_taken? in lib/position_taken.rb' do
