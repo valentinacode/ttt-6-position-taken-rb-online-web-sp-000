8c31bf1918f6e3e8e33616c5a59cd6ca636e4bf8
Valentina E <tiyahelfrih@gmail.com>
2020-05-20 16:34:37 +0000

    Automatically backed up by Learn

--- spec/position_taken_spec.rb
+++ spec/position_taken_spec.rb
@@ -1,5 +1,8 @@
 require_relative '../lib/position_taken.rb'
 
+def position_taken(input)
+  
+
 describe '#position_taken? in lib/position_taken.rb' do
   it 'accepts a board and the index to check as arguments' do
     board = []
